f4580b76381db7916f61ce92dd51b7900c06a99d
Angelo Alves <angelobehance@gmail.com>
2022-03-17 09:08:35 -0300

    FIX: adicionando o float / corrigir valores aproximados

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,5 @@
-let criptoName =''; 
-let finalCoinName='';
-
-// Quero que você agrupe todas as informações de cada moeda de entrada por objeto
-// Quero que você tenha todas as informações de cada moeda de saída por objeto
-// Desafio: fazer sem nem um if/else
+let criptoName = '';
+let finalCoinName = '';
 
 function httpGetAsync(theUrl, callback) {
     var xmlHttp = new XMLHttpRequest();
@@ -20,36 +16,40 @@ function getPrice(quantas, moedaEntrada, moedaSaida, callback) {
         callback(response[moedaEntrada][moedaSaida] * quantas)
     })
 }
-https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=brl
+// Exemplo da API \/ \/
+//https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=brl
 
-function transformaNomes(option,optionFinal){
-    switch(option){
-        case 'BTC': criptoName = 'bitcoin';break;
-        case 'ETH': criptoName = 'ethereum';break;
-        case 'PVU': criptoName = 'plant-vs-undead-token';break;
-        default:console.log("algo deu errado");
+//pega a seleção do input e transforma no ID necessário pra consumir a API
+function nameTransform(option, optionFinal) {
+    switch (option) {
+        case 'BTC': criptoName = 'bitcoin'; break;
+        case 'ETH': criptoName = 'ethereum'; break;
+        case 'PVU': criptoName = 'plant-vs-undead-token'; break;
+        default: console.log("algo deu errado");
     }
-    switch(optionFinal){
+    switch (optionFinal) {
         case 'BRL': finalCoinName = 'brl'; break;
-        case 'USD': finalCoinName = 'usd';break;
-        case 'EUR': finalCoinName = 'eur';break;
-        default:console.log("algo deu errado");
+        case 'USD': finalCoinName = 'usd'; break;
+        case 'EUR': finalCoinName = 'eur'; break;
+        default: console.log("algo deu errado");
     }
-
 }
+//formata o resultado final e o mostra na tela
+function FinalFormat(price) {
+    parseFloat(price)
+    document.getElementById('resultado').innerHTML = price.toLocaleString('pt-br', { style: 'currency', currency: finalCoinName });
+}
+
 function ativarBotao() {
     var select = document.getElementById('cripto');
     var option = select.options[select.selectedIndex].value;
     var selectFinal = document.getElementById('moeda-tradicional');
     var optionFinal = selectFinal.children[selectFinal.selectedIndex].value;
-    transformaNomes(option,optionFinal);
-    console.log(optionFinal)
+    nameTransform(option, optionFinal);
     var valorDigitado = document.querySelector('#valorEmCripto').value
-    getPrice(valorDigitado,criptoName,finalCoinName, price => {
-        document.getElementById('resultado').innerHTML = price.toLocaleString('pt-br',{style: 'currency', currency:finalCoinName});
-     })
+    getPrice(valorDigitado, criptoName, finalCoinName, price => {
+        FinalFormat(price)
+    })
+
 }
-/* 
-plant - vs - undead - token
-ethereum
-bitcoin */
+
